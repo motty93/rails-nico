5d709bed015fb9b7d353505fc094c4c55a818e23
takumi sasamoto <rdwbocungelt5@gmail.com>
2018-01-28 21:57:32 +0900

    nisenicoの命名規則変更

--- test/controllers/nise_nico_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class NiseNicoControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get nise_nico_index_url
-    assert_response :success
-  end
-
-end
