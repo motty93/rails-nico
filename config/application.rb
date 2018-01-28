724cc8b010a4f57687b7278fca308e537f4eab78
takumi sasamoto <rdwbocungelt5@gmail.com>
2018-01-28 19:55:38 +0900

    locateの設定

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,8 @@ module Nisenico
 				request_specs: false
 			g.fixture_replacement :factory_girl, dir: 'spec/factories'
 		end
+
+		config.i18n.default_locate = :ja
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
